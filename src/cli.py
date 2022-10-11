42474a26a16fb820b5739084b8b4fd02c72c8ac0
Manuel Fernandez <manfer.alonso@gmail.com>
2022-10-11 17:15:13 +0200

    creating CLI command to get stats

--- src/cli.py
+++ src/cli.py
@@ -1,10 +1,14 @@
 import typer
+from src.
 
 app = typer.Typer()
 
 
 @app.command()
-def hello():
+def show_statistics(arch: str = typer.Argument(
+    "amd64",
+    help="Architecture you want to check. Example: amd64, arm64, mips"
+)):
     typer.echo("Hello World")
 
 
