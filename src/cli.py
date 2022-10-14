884a17c695d2f65fe7d386607d763f7f28227291
Manuel Fernandez <manfer.alonso@gmail.com>
2022-10-14 02:34:34 +0200

    Add logic to cli

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,5 @@
 import typer
+from src.package_stats.stats import Stats
 
 app = typer.Typer()
 
@@ -8,13 +9,9 @@ def show_statistics(arch: str = typer.Argument(
     "amd64",
     help="Architecture you want to check. Example: amd64, arm64, mips"
 )):
-    typer.echo("Hello World")
-
-
-@app.command()
-def bye():
-    typer.echo("Bye World")
-
-
-if __name__ == "__main__":
-    app()
+    stats = Stats(arch=arch.lower())
+    stats.get_dist_files()
+    stats.get_contents()
+    stats.download_contents()
+    stats.count_packages()
+    stats.show_top_10()
